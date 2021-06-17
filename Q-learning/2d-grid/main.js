ff9fe9c8232dac8b23ac9647f8550af427d9a1c2
Luke <luke@luke.co.pl>
2021-06-17 02:04:21 +0200

    [Q-learning/2d-grid] Add simple implementation

--- Q-learning/2d-grid/main.js
+++ Q-learning/2d-grid/main.js
@@ -8,7 +8,11 @@ const ACTIONS = {
 const ENV_CONFIG = {
   rows: 10,
   cols: 10,
-  epsilon: 0.8,
+  learningRate: 0.8,
+  discountFactor: 0.99,
+  epsilon: 0.2,
+  maxMoves: 50,
+  nextMoveDelayMs: 0,
 };
 
 const ENV_STATE = {
@@ -41,14 +45,127 @@ const ENV_STATE = {
     s(7, 7),
   ],
   currentState: s(8, 6),
+  qValues: {},
+  moves: 0,
+  nextMoveDelayMs: ENV_CONFIG.nextMoveDelayMs,
 };
 
-function isRewarded(r, c) {
-  return reward(r, c) !== undefined;
+function chooseInitialState() {
+  while (1) {
+    const r = Math.floor(ENV_CONFIG.rows * Math.random());
+    const c = Math.floor(ENV_CONFIG.cols * Math.random());
+    const state = s(r, c);
+    if (
+      !ENV_STATE.obstacles.includes(state) &&
+      !ENV_STATE.absorbingStates.includes(state)
+    ) {
+      return state;
+    }
+  }
 }
 
-function reward(r, c) {
-  return ENV_STATE.rewardedStates[s(r, c)];
+function startLoop() {
+  (function loop() {
+    if (ENV_STATE.absorbingStates.includes(ENV_STATE.currentState)) {
+      console.log("Reached absorbing state, restarting");
+      resetState();
+    } else if (ENV_STATE.moves >= ENV_CONFIG.maxMoves) {
+      console.log(`Exceeded allowed ${ENV_CONFIG.maxMoves} moves, restarting`);
+      resetState();
+    } else {
+      performMove();
+    }
+    updateGrid();
+    setTimeout(loop, ENV_STATE.nextMoveDelayMs);
+  })();
+}
+
+function resetState() {
+  ENV_STATE.currentState = chooseInitialState();
+  ENV_STATE.moves = 0;
+}
+
+function performMove() {
+  const { currentState } = ENV_STATE;
+
+  const action = chooseAction(currentState);
+  const nextState = stateAfterAction(currentState, action);
+  if (nextState != currentState) {
+    const reward = rewardInState(nextState);
+    updateQValue(currentState, action, nextState, reward);
+  }
+  ENV_STATE.currentState = nextState;
+  updateGrid();
+
+  ENV_STATE.moves++;
+}
+
+function getQValue(state, a) {
+  return ENV_STATE.qValues[q(state, a)] || 0;
+}
+
+function updateQValue(state, action, nextState, reward) {
+  const { learningRate, discountFactor } = ENV_CONFIG;
+  const [_, bestNextActionValue] = bestActionWithValue(nextState);
+  const currentValue = getQValue(state, action);
+  ENV_STATE.qValues[q(state, action)] =
+    currentValue +
+    learningRate *
+      (reward + discountFactor * bestNextActionValue - currentValue);
+}
+
+function stateAfterAction(state, action) {
+  const [r, c] = getRowCol(state);
+  let newRow = r;
+  let newCol = c;
+  switch (action) {
+    case ACTIONS.UP:
+      newRow -= 1;
+      break;
+    case ACTIONS.RIGHT:
+      newCol += 1;
+      break;
+    case ACTIONS.DOWN:
+      newRow += 1;
+      break;
+    case ACTIONS.LEFT:
+      newCol -= 1;
+      break;
+  }
+  const newState = s(newRow, newCol);
+  if (
+    newRow < 0 ||
+    newRow >= ENV_CONFIG.rows ||
+    newCol < 0 ||
+    newCol >= ENV_CONFIG.cols ||
+    ENV_STATE.obstacles.includes(newState)
+  ) {
+    return state;
+  }
+  return newState;
+}
+
+function rewardInState(state) {
+  return ENV_STATE.rewardedStates[state] || 0;
+}
+
+function bestActionWithValue(state) {
+  return Object.values(ACTIONS)
+    .map((a) => [a, getQValue(state, a)])
+    .sort((x, y) => y[1] - x[1])[0];
+}
+
+function bestAction(state) {
+  const [bestNextAction, _] = bestActionWithValue(state);
+  return bestNextAction;
+}
+
+function chooseAction(state) {
+  if (Math.random() < ENV_CONFIG.epsilon) {
+    const actions = Object.values(ACTIONS);
+    return actions[Math.floor(Math.random() * actions.length)];
+  }
+  return bestAction(state);
 }
 
 function renderGrid(parent) {
@@ -61,7 +178,7 @@ function renderGrid(parent) {
       tr.insertAdjacentHTML(
         "beforeend",
         `
-                <td class="field state_${s(r, c)}">
+                <td data-state="${s(r, c)}" class="field state_${s(r, c)}">
                     <div class="reward"></div>
                     <div class="best-action"></div>
                 </td>`
@@ -76,6 +193,7 @@ function renderGrid(parent) {
   }
   table.appendChild(tr);
   parent.appendChild(table);
+
   updateGrid();
 }
 
@@ -112,7 +230,7 @@ function updateGrid() {
           field.classList.contains("absorbing") ||
           field.classList.contains("obstacle")
             ? ""
-            : actionSymbol(ACTIONS.RIGHT))
+            : actionSymbol(bestAction(field.dataset.state)))
     );
 }
 
@@ -133,4 +251,12 @@ function actionSymbol(action) {
 
 function s(r, c) {
   return `${r}_${c}`;
+}
+
+function q(state, a) {
+  return `${state}_${a}`;
+}
+
+function getRowCol(state) {
+  return state.split("_").map((x) => parseInt(x));
 }
